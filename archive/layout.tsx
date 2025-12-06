dfcae51742b8ca8de9af09f761c7b9b826f9e6ae
wiggam <georgewiggam8@gmail.com>
2025-12-06 00:37:02 -0500

    chore(app): remove archive, .next files

--- archive/layout.tsx
+++
@@ -1,9 +0,0 @@
-import { Suspense } from "react";
-
-export default function ChatLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <Suspense fallback={<div className="flex h-dvh" />}>
-      {children}
-    </Suspense>
-  );
-}
--- archive/page.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { KorliChat } from "@/components/korli-chat";
-
-export default function ChatPage() {
-  return <KorliChat />;
-}
